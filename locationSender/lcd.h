43667c540fc105777a213a8ce6c77fe6f7bb4aa5
Kal <kal9001@hotmail.com>
2018-09-04 14:23:17 +0100

    Create lcd.h

---
+++ locationSender/lcd.h
@@ -0,0 +1,20 @@
+/*
+ * lcd.h
+ *
+ * Created: 13/08/2018 15:36:55
+ *  Author: KAL90
+ */ 
+
+#ifndef LCD_H_
+#define LCD_H_
+
+char lcdLine1[17];
+char lcdLine2[17];
+
+void initLCD();
+void updateDisplay();
+
+
+
+
+#endif /* LCD_H_ */
